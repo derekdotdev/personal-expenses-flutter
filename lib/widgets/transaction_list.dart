cde6d4c91dc36aee5464a7a3beeff3da034b2d1f
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-04-04 09:10:53 -0400

    Remove default transactions and add a default image to be displayed when transactions list is empty.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -14,56 +14,63 @@ class TransactionList extends StatelessWidget {
       // ListView loads all children (even what's not visible)
       // ListView.builder() only loads what's visible on screen!
       // Builder is preferred
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  // Expense amount
-                  child: Text(
-                    '\$${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transactions[index].title,
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: const TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
+                const Text('No transactions added yet!'),
+                Image.asset('assets/images/waiting.png'),
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        // Expense amount
+                        child: Text(
+                          '\$${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transactions[index].title,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat.yMMMd().format(transactions[index].date),
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+              // Instead of a array of widgets, use map method (toList)
+              // to transform list of transaction objects into a list of widgets
+              // children: transactions.map((tx) {}).toList(),
             ),
-          );
-        },
-        itemCount: transactions.length,
-        // Instead of a array of widgets, use map method (toList)
-        // to transform list of transaction objects into a list of widgets
-        // children: transactions.map((tx) {}).toList(),
-      ),
     );
   }
 }
